709a87191c16fad21981b3690132c470757b6d0b
lucy harris <71592519+ljharris23@users.noreply.github.com>
2022-09-27 12:35:49 +0100

    cleanup

--- dsrnngan/run_eval.py
+++ dsrnngan/run_eval.py
@@ -9,17 +9,7 @@ import read_config
 read_config.set_gpu_mode()  # set up whether to use GPU, and mem alloc mode
 
 # input parameters
-# log_folder = '/ppdata/lucy-cGAN/logs/IFS/GAN/CL20fc-new-oro-xlong'; model_numbers = [563200]  # noqa
-# log_folder = '/ppdata/andrew-output/VAEGAN-xlong-100'; model_numbers = [550400]
-# log_folder = '/ppdata/lucy-cGAN/logs/det/best_model_hard_problem'; model_numbers = [524800]
-# log_folder = '/ppdata/andrew-output/GAN-equal-xlong'; model_numbers = [576000]
-# log_folder = '/ppdata/andrew-output/GAN-natural-xlong'; model_numbers = [531200]
-# log_folder = '/ppdata/andrew-output/finalGANnoCL'; model_numbers = [620800]
 log_folder = '/network/group/aopp/predict/TIP018_HARRIS_TENSORFL/andrew-output/rev-mainGAN'; model_numbers = [460800]
-# log_folder = '/network/group/aopp/predict/TIP018_HARRIS_TENSORFL/andrew-output/rev-mainVAEGAN'; model_numbers = [550400]
-# log_folder = '/network/group/aopp/predict/TIP018_HARRIS_TENSORFL/andrew-output/rev-GANnoCL'; model_numbers = [435200]
-# log_folder = '/network/group/aopp/predict/TIP018_HARRIS_TENSORFL/andrew-output/rev-GANnoconst'; model_numbers = [576000]
-
 val_years = 2020
 load_full_image = True
 
@@ -35,7 +25,6 @@ mode = setup_params["GENERAL"]["mode"]
 arch = setup_params["MODEL"]["architecture"]
 padding = setup_params["MODEL"]["padding"]
 batch_size = 1  # setup_params["TRAIN"]["batch_size"]
-# steps_per_checkpoint = setup_params["TRAIN"]["steps_per_checkpoint"]
 problem_type = setup_params["GENERAL"]["problem_type"]
 filters_gen = setup_params["GENERATOR"]["filters_gen"]
 noise_channels = setup_params["GENERATOR"]["noise_channels"]
@@ -60,14 +49,7 @@ if mode in ["GAN", "VAEGAN"]:
 elif mode == "det":
     rank_samples = 1
 
-if add_noise is False and load_full_image is False:
-    out_fn = "{}/eval_no_noise__{}.txt".format(log_folder, str(val_years))
-elif add_noise is True and load_full_image is False:
-    out_fn = "{}/eval_noise__{}.txt".format(log_folder, str(val_years))
-if add_noise is False and load_full_image is True:
-    out_fn = "{}/eval_no_noise_full_image__{}.txt".format(log_folder, str(val_years))
-elif add_noise is True and load_full_image is True:
-    out_fn = "{}/eval_noise_full_image__{}.txt".format(log_folder, str(val_years))
+out_fn = "{}/eval_{}.txt".format(log_folder, str(val_years))
 
 evaluation.rank_metrics_by_time(mode=mode,
                                 arch=arch,
